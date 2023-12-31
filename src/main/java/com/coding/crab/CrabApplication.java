2c510c8fb7842f345730ff4afd53d7368791638d
djdjdddd <djdj2297@gmail.com>
2023-12-31 23:40:01 +0900

    Feat: 스프링 부트 프로젝트 생성 및 dependency 추가

---
+++ src/main/java/com/coding/crab/CrabApplication.java
@@ -0,0 +1,13 @@
+package com.coding.crab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding/crab/CrabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.crab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
