443bbea991a8c68cee7945b09ce2a11709c3ce38
djdjdddd <djdj2297@gmail.com>
2023-12-31 23:41:45 +0900

    Feat: Temp.java

---
+++ src/test/java/com/coding/crab/temp/TempTest.java
@@ -0,0 +1,13 @@
+package com.coding.crab.temp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TempTest {
+    
+    @Test
+    public void 테스트(){
+        System.out.println("true = " + true);
+    }
+}
