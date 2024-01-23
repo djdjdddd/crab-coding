25e4c67219a4a31de66a1119635733a43a31ef5e
Ohsesol <tpthf1@gmail.com>
2024-01-24 00:02:07 +0900

    [Chore] deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 sudo docker ps -a -q --filter "name=app" | grep -q . && docker stop app && docker rm app | true
 
-sudo docker rmi pgmjun/cicdstudy:latest
+sudo docker rmi ohsesol/crab-coding-spring
 
-sudo docker pull pgmjun/cicdstudy:latest
+sudo docker pull ohsesol/crab-coding-spring
 
-docker run -d -p 8080:8080 --env-file=env_list.txt --name app ohsesol/crab-coding-spring:latest
+docker run -d -p 8080:8080 --env-file=env_list.txt --name app ohsesol/crab-coding-spring
 
 docker rmi -f $(docker images -f "dangling=true" -q) || true
