c7813b6ff3645594dace2bfaf215b40772a6a828
djdjdddd <djdj2297@gmail.com>
2024-02-02 23:37:50 +0900

    Docs: 실제 Swagger 적용한 Controller 예시

--- src/main/java/com/coding/crab/domain/signup/controller/SignupController.java
+++ src/main/java/com/coding/crab/domain/signup/controller/SignupController.java
@@ -3,11 +3,16 @@ package com.coding.crab.domain.signup.controller;
 import com.coding.crab.api.mail.MailDTO;
 import com.coding.crab.api.mail.MailService;
 import com.coding.crab.domain.signup.service.SignupService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "회원가입", description = "설명란")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -18,13 +23,13 @@ public class SignupController {
 
     private final SignupService signupService;
 
-    @RequestMapping("/mail-test")
+    @PostMapping("/mail-test")
     public void mailTest(){
         log.info("mail-test 요청 들어옴");
-        
+
         MailDTO mailDTO = new MailDTO();
         mailDTO.setUserName("김용희");
-        mailDTO.setUserMail("jisu3148496@naver.com");
+//        mailDTO.setUserMail("jisu3148496@naver.com");
 //        mailDTO.setUserMail("djdjdddd@khu.ac.kr");
 //        mailDTO.setUserMail("hjmin1221@naver.com");
 
