ac64b4aebce8f6f1f2e27ed38de1ae9b926a0d9c
Ohsesol <tpthf1@gmail.com>
2024-02-07 00:55:06 +0900

    Test: 젠킨스 테스트 시 contextLoads() FAILED 해결

--- src/test/java/com/coding/crab/CrabApplicationTests.java
+++ src/test/java/com/coding/crab/CrabApplicationTests.java
@@ -3,7 +3,7 @@ package com.coding.crab;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CrabApplicationTests.class)
 class CrabApplicationTests {
 
 	@Test
